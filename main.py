b31caa77db64728704397c02d4a5078f2b93f6da
DEEPTHI PRAKASH <52069060+deepthi2105@users.noreply.github.com>
2024-10-27 11:54:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 def min_coins(coins, target_amount):
-    ""code here""
+    dp = [float('inf')] * (target_amount + 1)
+    dp[0] = 0 
+    for coin in coins:
+        for amount in range(coin, target_amount + 1):
+            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+
+    return dp[target_amount] if dp[target_amount] != float('inf') else -1
